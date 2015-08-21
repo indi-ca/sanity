9b20d164e7e3423934075c3b9fa87ecb34b52e4e
Indika Piyasena <indika@hydrasqr.com>
2015-08-21 21:52:22 +1000

    some better scripts

--- responder/Main.hs
+++ responder/Main.hs
@@ -12,6 +12,7 @@ import System.Log.Handler.Syslog
 -- import System.Log.Handler (setFormatter)
 -- import System.Log.Formatter
 
+import Desktop (activate, switchDesktop)
 
 type Msg = (Int, String)
 
@@ -38,12 +39,21 @@ mainLoop sock chan nr = do
     runConn conn chan 0
     mainLoop sock chan $! nr+1
 
+
+perform :: String -> IO ()
+perform action = do
+    switchDesktop 2
+    activate "Slack"
+    return ()
+
+
 runConn :: (Socket, SockAddr) -> Chan Msg -> Int -> IO ()
 runConn (sock, _) chan nr = do
     let broadcast msg = writeChan chan (nr, msg)
     hdl <- socketToHandle sock ReadWriteMode
     hSetBuffering hdl NoBuffering
     action <- liftM init (hGetLine hdl)
+    perform action
     debugM "Sanity.Responder" $ "Got request: " ++  action
     hPutStrLn hdl action
     hPutStrLn hdl "OK"
---
+++ scripts/activate.scpt
@@ -0,0 +1,4 @@
+on run argv
+    set theProgram to (item 1 of argv)
+    tell application theProgram to activate
+end run
